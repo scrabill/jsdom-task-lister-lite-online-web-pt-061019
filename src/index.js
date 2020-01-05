507da602c5ca600976ab10db95d10217e3d4494e
scrabill <shannon.crabill@gmail.com>
2020-01-04 20:25:09 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const formField = document.getElementById('new-task-description');
+  const submit = document.getElementById('submit');
+  const ul = document.getElementById('tasks');
+
+  submit.addEventListener("click", function(event) {
+      let text = formField.value; // Pizza
+
+      createListItem(text);
+
+      event.preventDefault();
+  }, false);
+
+  function createListItem(text) {
+    const li = document.createElement("li");
+
+    li.innerText = text;
+    ul.appendChild(li);
+  }
+
 });
